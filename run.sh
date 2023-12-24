b0abadcf0a9d6cca1fc68021888f3e1d41813e12
NathanielSong <contact@rainbowcreation.net>
2023-12-24 13:52:29 +0700

    update buildscript

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 filename="RainBowCreation"
 dir="target"
+lite="lite"
 prefix="v1_"
 mkdir $dir
+cd $dir
+echo "clearing old version"
+rm -rf *
+cd ..
+echo "compiling full version.."
 for V in 8 9 11 13 15 17 18 19 20
 do
     file="${prefix}${V}"
@@ -13,3 +19,16 @@ do
     cd ..
 done
 echo "done"
+echo "compiling lite version"
+cd $lite
+for V in 8 9 11 13 15 17 18 19 20
+do
+    file="${prefix}${V}"
+    zipped="${filename}_${file}_${lite}.zip"
+    echo "compressing ${file}..."
+    cd ${file}
+    zip -r "../${dir}/$zipped" *
+    cd ..
+done
+cd ..
+echo "done :)
