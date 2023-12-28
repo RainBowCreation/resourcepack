cf2de074d84f904aa4bec4a2da4b1803b9057eff
NathanielSong <contact@rainbowcreation.net>
2023-12-28 18:55:21 +0700

    remove _l version

--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@ filename="RainBowCreation"
 dir="target"
 lite="lite"
 prefix="v1_"
-lobby="l"
 support_versions=(8 9 11 13 15 17 18 19 20)
 rm -rf "${dir}"
 mkdir "${dir}"
-gui="assets/minecraft/textures/gui"
+minecraft="assets/minecraft"
+texture="${minecraft}/textures"
+gui="${texture}/gui"
 container="${gui}/container"
 title="${gui}/title"
 list=(
@@ -17,55 +18,45 @@ list=(
     "${title}/minecraft.png"
     "${title}/edition.png"
 )
-llist=(
-    "${container}/generic_54"
+list_13=(
+    "${minecraft}/font"
+    "${minecraft}/models/item"
+    "${texture}/custom"
+)
+list_19=(
+    "atlases"
+    "atlates"
 )
 CP() {
     mkdir -p $(dirname "$2") && cp -r "$1" "$2"
+}
+LI() {
+    for F in "${1[@]}"
+        echo "copying ${F}.."
+        CP  "${F}" "${lite}/${F}"
 }
 for V in "${support_versions[@]}"
 do
     file="${prefix}${V}"
     echo "compressing ${file}..."
     cd "${file}"
-    echo "create cache file for lite version.."
+    echo "create cache file for ${lite} version.."
     mkdir "${lite}"
-    for F in "${list[@]}"
-    do
-        echo "copying ${F}.."
-        CP  "${F}" "${lite}/${F}"
-    done
+    LI "${list}"
+    if ${V} > 11
+        LI "${list_13}"
+    fi
+    if ${V} > 18
+        LI "${list_19}"
+    fi
     echo "compressing ${lite} of ${file}.."
     cd "${lite}"
     zipped="${filename}_${file}"
     zip -qr "../../${dir}/${zipped}_${lite}.zip" *
     cd ..
-    for L in "${llist[@]}"
-    do
-        echo "copying ${L}.."
-        CP "${L}_${lobby}.png" "${lite}/${L}.png"
-    done
-    echo "compressing ${lobby}_${lite} of ${file}.."
-    cd "${lite}"
-    zip -qr "../../${dir}/${zipped}_${lobby}_${lite}.zip" *
-    cd ..
     rm -rf "${lite}"
     echo "compressing full version of ${file}.."
     zip -qr "../${dir}/${zipped}.zip" *
-    echo "compressing ${lobby} version of ${file}.."
-    for L in "${llist[@]}"
-    do
-        mv "${L}.png" "${L}_t.png"
-        mv "${L}_${lobby}.png" "${L}.png"
-    done
-    zip -qr "../${dir}/${zipped}_${lobby}.zip" *
-    echo "clear tmp file.."
-    for L in "${llist[@]}"
-    do
-        mv "${L}.png" "${L}_${lobby}.png"
-        mv "${L}_t.png" "${L}.png"
-    done
-    cd ..
 done
 echo "compressing bedrock version.."
 cp "${filename}.mcpack" "${dir}/"
